5674fc309a85999613d84fc40d5b7b1ac545c3a6
Tapas Mondal <mondaltapas02@gmail.com>
2020-02-05 11:27:10 +0530

    Add files via upload

---
+++ Validation/index.js
@@ -0,0 +1,6 @@
+import checkNoSpecialChar from "./checkNoSpecialChar";
+import checkExactLength from "./checkExactLength";
+import checkPattern from "./checkPattern";
+import checkOnSubmit from "./checkOnSubmit";
+
+export { checkNoSpecialChar, checkExactLength, checkPattern, checkOnSubmit };
